28918b31452f0081d14f0e8a3f45b87c4316a8d8
Larry Garfield <larry@garfieldtech.com>
2015-08-20 20:19:35 -0500

    Update exception hierarchy.

--- InvalidArgumentException.php
+++ InvalidArgumentException.php
@@ -8,4 +8,4 @@ namespace Psr\Cache;
  * Any time an invalid argument is passed into a method it must throw an
  * exception class which implements Psr\Cache\InvalidArgumentException.
  */
-interface InvalidArgumentException { }
+interface InvalidArgumentException extends CacheException { }
