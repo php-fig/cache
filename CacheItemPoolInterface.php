42299d546acc691a2f0c6fa4ebc70095aab58bc2
Larry Garfield <larry@garfieldtech.com>
2015-12-02 00:34:28 -0600

    Resync with the final changes to the spec.

--- CacheItemPoolInterface.php
+++ CacheItemPoolInterface.php
@@ -3,30 +3,25 @@
 namespace Psr\Cache;
 
 /**
- * \Psr\Cache\CacheItemPoolInterface generates Cache\CacheItem objects.
- *
- * The primary purpose of Cache\CacheItemPoolInterface is to accept a key from the
- * Calling Library and return the associated Cache\CacheItemInterface object.
- * It is also the primary point of interaction with the entire cache collection.
- * All configuration and initialization of the Pool is left up to an Implementing
- * Library.
+ * CacheItemPoolInterface generates CacheItemInterface objects.
  */
 interface CacheItemPoolInterface
 {
-
     /**
      * Returns a Cache Item representing the specified key.
      *
-     * This method must always return an ItemInterface object, even in case of
+     * This method must always return a CacheItemInterface object, even in case of
      * a cache miss. It MUST NOT return null.
      *
      * @param string $key
      *   The key for which to return the corresponding Cache Item.
-     * @return \Psr\Cache\CacheItemInterface
-     *   The corresponding Cache Item.
-     * @throws \Psr\Cache\InvalidArgumentException
+     *
+     * @throws InvalidArgumentException
      *   If the $key string is not a legal value a \Psr\Cache\InvalidArgumentException
      *   MUST be thrown.
+     *
+     * @return CacheItemInterface
+     *   The corresponding Cache Item.
      */
     public function getItem($key);
 
@@ -35,29 +30,73 @@ interface CacheItemPoolInterface
      *
      * @param array $keys
      * An indexed array of keys of items to retrieve.
+     *
+     * @throws InvalidArgumentException
+     *   If any of the keys in $keys are not a legal value a \Psr\Cache\InvalidArgumentException
+     *   MUST be thrown.
+     *
      * @return array|\Traversable
-     * A traversable collection of Cache Items keyed by the cache keys of
-     * each item. A Cache item will be returned for each key, even if that
-     * key is not found. However, if no keys are specified then an empty
-     * traversable MUST be returned instead.
+     *   A traversable collection of Cache Items keyed by the cache keys of
+     *   each item. A Cache item will be returned for each key, even if that
+     *   key is not found. However, if no keys are specified then an empty
+     *   traversable MUST be returned instead.
      */
     public function getItems(array $keys = array());
 
     /**
+     * Confirms if the cache contains specified cache item.
+     *
+     * Note: This method MAY avoid retrieving the cached value for performance reasons.
+     * This could result in a race condition with CacheItemInterface::get(). To avoid
+     * such situation use CacheItemInterface::isHit() instead.
+     *
+     * @param string $key
+     *    The key for which to check existence.
+     *
+     * @throws InvalidArgumentException
+     *   If the $key string is not a legal value a \Psr\Cache\InvalidArgumentException
+     *   MUST be thrown.
+     *
+     * @return bool
+     *  True if item exists in the cache, false otherwise.
+     */
+    public function hasItem($key);
+
+    /**
      * Deletes all items in the pool.
      *
-     * @return boolean
+     * @return bool
      *   True if the pool was successfully cleared. False if there was an error.
      */
     public function clear();
 
     /**
+     * Removes the item from the pool.
+     *
+     * @param string $key
+     *   The key for which to delete
+     *
+     * @throws InvalidArgumentException
+     *   If the $key string is not a legal value a \Psr\Cache\InvalidArgumentException
+     *   MUST be thrown.
+     *
+     * @return bool
+     *   True if the item was successfully removed. False if there was an error.
+     */
+    public function deleteItem($key);
+
+    /**
      * Removes multiple items from the pool.
      *
      * @param array $keys
-     * An array of keys that should be removed from the pool.
-     * @return static
-     * The invoked object.
+     *   An array of keys that should be removed from the pool.
+
+     * @throws InvalidArgumentException
+     *   If any of the keys in $keys are not a legal value a \Psr\Cache\InvalidArgumentException
+     *   MUST be thrown.
+     *
+     * @return bool
+     *   True if the items were successfully removed. False if there was an error.
      */
     public function deleteItems(array $keys);
 
@@ -67,8 +106,8 @@ interface CacheItemPoolInterface
      * @param CacheItemInterface $item
      *   The cache item to save.
      *
-     * @return static
-     *   The invoked object.
+     * @return bool
+     *   True if the item was successfully persisted. False if there was an error.
      */
     public function save(CacheItemInterface $item);
 
@@ -77,8 +116,9 @@ interface CacheItemPoolInterface
      *
      * @param CacheItemInterface $item
      *   The cache item to save.
-     * @return static
-     *   The invoked object.
+     *
+     * @return bool
+     *   False if the item could not be queued or if a commit was attempted and failed. True otherwise.
      */
     public function saveDeferred(CacheItemInterface $item);
 
@@ -86,7 +126,7 @@ interface CacheItemPoolInterface
      * Persists any deferred cache items.
      *
      * @return bool
-     *   TRUE if all not-yet-saved items were successfully saved. FALSE otherwise.
+     *   True if all not-yet-saved items were successfully saved or there were none. False otherwise.
      */
     public function commit();
 
--- InvalidArgumentException.php
+++ InvalidArgumentException.php
@@ -3,9 +3,11 @@
 namespace Psr\Cache;
 
 /**
- * Exception interface for invalid cache arguements.
+ * Exception interface for invalid cache arguments.
  *
  * Any time an invalid argument is passed into a method it must throw an
  * exception class which implements Psr\Cache\InvalidArgumentException.
  */
-interface InvalidArgumentException extends CacheException { }
+interface InvalidArgumentException extends CacheException
+{
+}
