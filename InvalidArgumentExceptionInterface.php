fa7e3935f72a1772c32ff827396a875ccccd6dfe
Aaron Scherer <aequasi@gmail.com>
2015-12-05 22:52:47 +0100

    Renaming interfaces to include suffix

--- InvalidArgumentExceptionInterface.php
+++ InvalidArgumentExceptionInterface.php
@@ -6,8 +6,8 @@ namespace Psr\Cache;
  * Exception interface for invalid cache arguments.
  *
  * Any time an invalid argument is passed into a method it must throw an
- * exception class which implements Psr\Cache\InvalidArgumentException.
+ * exception class which implements Psr\Cache\InvalidArgumentExceptionInterface.
  */
-interface InvalidArgumentException extends CacheException
+interface InvalidArgumentExceptionInterface extends CacheExceptionInterface
 {
 }
