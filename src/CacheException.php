8ec0d790661197af4eecb9d1325016962e045a58
azjezz <azjezz@azjezz.me>
2018-09-22 02:34:54 +0100

    Update CacheException.php

--- src/CacheException.php
+++ src/CacheException.php
@@ -2,9 +2,11 @@
 
 namespace Psr\Cache;
 
+use Throwable;
+
 /**
  * Exception interface for all exceptions thrown by an Implementing Library.
  */
-interface CacheException
+interface CacheException extends Throwable
 {
 }
